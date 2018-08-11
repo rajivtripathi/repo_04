71f677f6608b0621f0950cf9dfa6e03d926dcb45
rajiv42 <rajivtripathi.july10@gmail.com>
2018-08-11 13:47:28 +0530

    1st commit done

---
+++ Demo.java
@@ -0,0 +1,14 @@
+package maven_Project;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+@Test
+public static void Maven(){
+		
+		System.out.println("Welcome to Maven world------we are here");
+		System.out.println("------This is second method ----");
+	}
+
+}
